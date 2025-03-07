2504a8102350488aeefae40ad0ca6298d62f9b6d
fisker Cheung <lionkay@gmail.com>
2025-03-08 02:56:52 +0800

    chore: update dependencies (#304)

---
+++ scripts/generate.mjs
@@ -0,0 +1,108 @@
+import fs from 'node:fs/promises'
+import * as prettier from 'prettier'
+import { outdent } from 'outdent'
+import { Charset } from 'regexp-util'
+import packageJson from '../package.json' with { type: 'json' }
+
+const unicodeDataPackageName = Object.keys(packageJson.devDependencies).find(
+  name => name.startsWith('@unicode/unicode-'),
+)
+
+const { default: unicodeData } = await import(unicodeDataPackageName)
+
+const unSupportedCategories = new Set([
+  'Names',
+  'Sequence_Property',
+  'Special_Casing',
+])
+const sourceDirectory = new URL('../src/', import.meta.url)
+const categories = Object.entries(unicodeData).filter(
+  ([category]) => !unSupportedCategories.has(category),
+)
+
+async function writeFile(file, code) {
+  const directory = new URL('./', file)
+  await fs.mkdir(directory, { recursive: true })
+  await fs.writeFile(
+    file,
+    await prettier.format(code, { parser: 'typescript' }),
+  )
+}
+
+/* ----------------------------- types.generated ---------------------------- */
+
+const typesFilename = new URL('./types.generated.ts', sourceDirectory)
+await writeFile(
+  typesFilename,
+  outdent`
+    export interface Category {
+      ${categories
+        .map(([category, subCategories]) => {
+          const types =
+            subCategories.length === 0
+              ? 'never'
+              : `(${subCategories
+                  .map(subCategory => `| ${JSON.stringify(subCategory)}`)
+                  .join('\n')})[]`
+          return `${JSON.stringify(category)}: ${types};`
+        })
+        .join('\n')}
+    };
+  `,
+)
+
+/* ----------------------------- data.generated ----------------------------- */
+
+const dataDirectory = new URL('./data.generated/', sourceDirectory)
+
+await fs.rm(dataDirectory, { recursive: true, force: true })
+
+await writeFile(
+  new URL('./index.ts', dataDirectory),
+  categories
+    .map(
+      ([subCategory]) =>
+        `export * as ${subCategory} from './${subCategory}/index.js';`,
+    )
+    .join('\n'),
+)
+
+for (const [category, subCategories] of categories) {
+  const categoryDirectory = new URL(`./${category}/`, dataDirectory)
+
+  await writeFile(
+    new URL('./index.ts', categoryDirectory),
+    subCategories
+      .map(
+        subCategory =>
+          `export { default as ${subCategory} } from './${subCategory}.js';`,
+      )
+      .join('\n'),
+  )
+
+  for (const subCategory of subCategories) {
+    const filename = new URL(`./${subCategory}.ts`, categoryDirectory)
+    let content = new Charset()
+
+    const { default: data } = await import(
+      `${unicodeDataPackageName}/${category}/${subCategory}/code-points.js`
+    )
+    const batch = 1000
+    for (let i = 0; i < data.length; i += batch) {
+      content = content.union(...data.slice(i, i + batch))
+    }
+
+    await writeFile(
+      filename,
+      outdent`
+        const _: (number | [number, number])[] = ${JSON.stringify(
+          content.data.map(([start, end]) =>
+            start === end ? start : [start, end],
+          ),
+        )};
+
+        export default _;
+      `,
+    )
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { Charset } from 'regexp-util'
-import { Category } from './types.generated.js'
+import type { Category } from './types.generated.js'
 import * as data from './data.generated/index.js'
 
 export default function unicode(categories: Partial<Category>): Charset {
-  const keys = Object.keys(categories) as Array<keyof Category>
+  const keys = Object.keys(categories) as (keyof Category)[]
 
   if (keys.length === 0) {
     throw new Error(`Expected at least one category, but received 0.`)
@@ -18,21 +18,25 @@ export default function unicode(categories: Partial<Category>): Charset {
     throw new Error(`Expected at least one sub category, but received 0.`)
   }
 
-  const charsets = keys.map(key => {
-    const subCategories: Array<Category[typeof key][number]> = categories[key]!
-    const subCharsets = subCategories.map(_ => getCharset(key, _))
+  const charsets = keys.map(category => {
+    const subCategories: Category[typeof category][number][] =
+      categories[category]!
+    const subCharsets = subCategories.map(subCategory =>
+      getCharset(category, subCategory),
+    )
     return new Charset().union(...subCharsets)
   })
 
   return charsets.reduce((a, b) => a.intersect(b))
 }
 
-function getCharset<C extends keyof Category>(
-  category: C,
-  subCategory: Category[C][number],
+function getCharset<CategoryName extends keyof Category>(
+  category: CategoryName,
+  subCategory: Category[CategoryName][number],
 ) {
   const categoryData = data[category]
-  const charsetInputs: Array<number | [number, number]> =
-    categoryData[subCategory as keyof typeof categoryData]
+  const charsetInputs = categoryData[
+    subCategory as keyof typeof categoryData
+  ] as (number | [number, number])[]
   return new Charset().union(...charsetInputs)
 }
